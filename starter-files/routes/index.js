3b6064ebcfe227d3b0a580249a7e9152ccab491b
rebelBeard <rfm144@gmail.com>
2017-05-11 22:49:46 -0400

    experimented with req and res

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+	const wes = { name: 'Wes', age: 100, cool: true };
+	res.json(req.query);
+  // res.send('Hey! It works!');
+  // res.json(wes);
+});
+
+router.get('/reverse/:name', (req, res) => {
+	const reverse = [...req.params.name].reverse().join('');
+	res.send(reverse);
 });
 
 module.exports = router;
