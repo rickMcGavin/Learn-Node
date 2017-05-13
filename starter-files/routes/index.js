39184203dc701672bc859119ddfb5598682e1363
rebelBeard <rfm144@gmail.com>
2017-05-12 22:23:27 -0400

    core concepts complete

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-	const wes = { name: 'Wes', age: 100, cool: true };
-	res.json(req.query);
-  // res.send('Hey! It works!');
-  // res.json(wes);
-});
-
-router.get('/reverse/:name', (req, res) => {
-	const reverse = [...req.params.name].reverse().join('');
-	res.send(reverse);
-});
+router.get('/',  storeController.myMiddleware, storeController.homePage)
 
 module.exports = router;
